213ab54291eb89733f3acfc1dcd1cee7133b2091
Tawaf Thasin Talukder <tawaf4fg@gmail.com>
2026-04-17 10:59:23 +0600

    Add files via upload

---
+++ displayingtext2.py
@@ -0,0 +1,19 @@
+print('I am Tawaf Thasin Talukder')
+print('I just started my python coding')
+print('I took admission in NORTH SOUTH UNIVERSITY')
+
+
+#double quote helps me to put a apostrophe on the coding line
+print("I'm a student of NSU")
+
+
+#\n helps me to put the word in the next line
+print('Tawaf lives in \nBangladesh')
+ 
+ 
+#i can use + sign to add 2 lines together
+print('I love to eat chocolates'+' and I love to play games')
+
+
+#if i use one \ it will show me the word in downward but if i use \\ it will show me beside the line
+print('How can I get the \\news?')
